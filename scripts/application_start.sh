ca485332413177615aa72771669e274b65e6d698
João Mello <joao.mello@olxbr.com>
2024-06-18 13:57:11 -0300

    feat: Code deploy fix

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,8 +2,13 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/deploy_logs/deploy.log
 
+echo 'enter folder' >> /home/ec2-user/deploy_logs/deploy.log
+cd /home/ec2-user/chat
+
 echo 'stop docker' >> /home/ec2-user/deploy_logs/deploy.log
 sudo docker-compose down >> /home/ec2-user/deploy_logs/deploy.log
 
 echo 'restart docker' >> /home/ec2-user/deploy_logs/deploy.log
 sudo docker-compose up -d >> /home/ec2-user/deploy_logs/deploy.log
+
+echo 'docker restarted' >> /home/ec2-user/deploy_logs/deploy.log
